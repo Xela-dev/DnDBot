ec8cc320cc157243a94fa3130c72cca1dc7a8f82
Aleksandra Nosińska <aleks.nosinska@gmail.com>
2025-02-11 17:32:26 +0100

    Modified DnDService

--- DnDBot/Service/DnDService.cs
+++ DnDBot/Service/DnDService.cs
@@ -13,19 +13,19 @@ public class DnDService
         return apiService.GetItemByNameAsync("backgrounds", name);
     }
     
-    public Task<string> GetRaceByName(string name)
+    public Task<Race> GetRaceByName(string name)
     {
-        return apiService.GetItemByNameAsync("races", name);
+        return apiService.GetItemByNameAsync<Race>("races", name);
     }
     
-    public Task<string> GetClassByNameAsync(string name)
+    public Task<PersonClass> GetClassByNameAsync(string name)
     {
-        return apiService.GetItemByNameAsync("classes", name);
+        return apiService.GetItemByNameAsync<PersonClass>("classes", name);
     }
     
-    public Task<string> GetSubraceByNameAsync(string name)
+    public Task<Subrace> GetSubraceByNameAsync(string name)
     {
-        return apiService.GetItemByNameAsync("subraces", name);
+        return apiService.GetItemByNameAsync<Subrace>("subraces", name);
     }
     
     public Task<string> GetByTraitsNameAsync(string name)
@@ -33,9 +33,9 @@ public class DnDService
         return apiService.GetItemByNameAsync("traits", name);
     }
     
-    public Task<string> GetSpellByNameAsync(string name)
+    public Task<Spell> GetSpellByNameAsync(string name)
     {
-        return apiService.GetItemByNameAsync("spells", name);
+        return apiService.GetItemByNameAsync<Spell>("spells", name);
     }
     
     public Task<string> GetAlignmentByNameAsync(string alignmentOrder, string morality = "")
@@ -44,24 +44,24 @@ public class DnDService
         return apiService.GetItemByNameAsync("ability-scores", name);
     }
 
-    public async Task<string> GetAbilityScoreByNameAsync(string name)
+    public async Task<AbilityScore> GetAbilityScoreByNameAsync(string name)
     {
-        return await apiService.GetItemByNameAsync("ability-scores", name);
-    }
-
-    public async Task<string> GetSkillByIndexAsync(string index)
-    {
-        return await apiService.GetItemByNameAsync("skills", index);
+        return await apiService.GetItemByNameAsync<AbilityScore>("ability-scores", name);
     }
     
-    public async Task<string> GetLanguagesByNameAsync(string name)
+    public async Task<Skill> GetSkillByIndexAsync(string index)
     {
-        return await apiService.GetItemByNameAsync("languages", name);
+        return await apiService.GetItemByNameAsync<Skill>("skills", index);
+    }
+    
+    public async Task<Language> GetLanguagesByNameAsync(string name)
+    {
+        return await apiService.GetItemByNameAsync<Language>("languages", name);
     }
 
-    public async Task<string> GetEquipmentCategoryByNameAsync(string name)
+    public async Task<EquipmentCategory> GetEquipmentCategoryByNameAsync(string name)
     {
-        return await apiService.GetItemByNameAsync("equipment-categories", name);
+        return await apiService.GetItemByNameAsync<EquipmentCategory>("equipment-categories", name);
     }
     
     public async Task<string> GetResourcesByPathAsync(string path)
